a226a0b3830060e42d3928ba1f4b5e83f71b5312
leo <lior_yarkon@hotmail.com>
2019-08-03 22:44:02 +0300

    minor view adjustments

--- react-client/src/views/MainTblView/SingleRangeEmployees/SingleRangeEmployees.js
+++ react-client/src/views/MainTblView/SingleRangeEmployees/SingleRangeEmployees.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './SingleRangeEmployees.scss';
 import TblHeader from './TblHeader';
+import {MAX_EMPLOYEES_IN_VIEW} from '../../../consts';
 
 const SingleRangeEmployees = props => {
     const EmpsToDispaly = props.empsToDisplay;
-    const NumItemsToDispaly = EmpsToDispaly.length;
     const GetColorByIndex = i => {
         const OneBasedIndex = i + 1;
         return OneBasedIndex % 2 !== 0 ? '#fff' : '#f7fafe';
@@ -17,14 +17,14 @@ const SingleRangeEmployees = props => {
                     <div 
                         key={'_emp-' + item.id}
                         className='d-flex emp-record'
-                        style={{height: NumItemsToDispaly + '%', backgroundColor: GetColorByIndex(i)}}
+                        style={{height: MAX_EMPLOYEES_IN_VIEW + '%', backgroundColor: GetColorByIndex(i)}}
                     >
                         <div 
                             id='id-item' 
                             className='emp-item d-flex justify-content-center align-items-center' 
                             style={{width: 6 + '%'}}
                         >
-                            {item.id}
+                            {item.id + 1}
                         </div>
                         <div className='emp-item d-flex align-items-center' style={{width: 9 + '%'}}>{item.firstName}</div>
                         <div className='emp-item d-flex align-items-center' style={{width: 9 + '%'}}>{item.lastName}</div>

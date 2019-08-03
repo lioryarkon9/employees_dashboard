f649fdaeed945acc2de8126594bc2cfd52b5e689
leo <lior_yarkon@hotmail.com>
2019-08-03 09:58:15 +0300

    done with header banner. todo: images

---
+++ react-client/src/components/BannerItem/BannerItem.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './BannerItem.scss';
+
+const BannerItem = props => {
+    return (
+        <div className='banner-item d-flex'>
+            <div className='header-banner-icon'>
+                <div className='banner-img-container d-flex align-items-center'>
+                    <span>todo:img</span>
+                </div>
+            </div>
+            <div className='header-banner-content'>
+                <div className='banner-figure'>
+                    {props.figure}
+                </div>
+                <div className='banner-txt'>
+                    {props.headline}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default BannerItem;
--- react-client/src/views/HeaderBanner/HeaderBanner.js
+++ react-client/src/views/HeaderBanner/HeaderBanner.js
@@ -1,9 +1,59 @@
 import React from 'react';
+import './HeaderBanner.scss';
+import BannerItem from '../../components/BannerItem';
 
 const HeaderBanner = props => {
     return (
-        <div>
-            todo: HeaderBanner
+        <div id='header-banner' className='d-flex align-items-center'>
+            <div 
+                style={{borderRight: 1 + 'px solid'}}
+                className='item flex-fill d-flex justify-content-center'
+            >
+                <BannerItem
+                    imgSrc=""
+                    figure="1000"
+                    headline="Headline1"
+                />
+            </div>
+            <div 
+                style={{borderRight: 1 + 'px solid'}}
+                className='item flex-fill d-flex justify-content-center'
+            >
+                <BannerItem
+                    imgSrc=""
+                    figure="837"
+                    headline="Headline2"
+                />
+            </div>
+            <div 
+                style={{borderRight: 1 + 'px solid'}}
+                className='item flex-fill d-flex justify-content-center'
+            >
+                <BannerItem
+                    imgSrc=""
+                    figure="342"
+                    headline="Headline3"
+                />
+            </div>
+            <div 
+                style={{borderRight: 1 + 'px solid'}}
+                className='item flex-fill d-flex justify-content-center'
+            >
+                <BannerItem
+                    imgSrc=""
+                    figure="473"
+                    headline="Headline4"
+                />
+            </div>
+            <div 
+                className='item flex-fill d-flex justify-content-center'
+            >
+                <BannerItem
+                    imgSrc=""
+                    figure="123"
+                    headline="Headline5"
+                />
+            </div>
         </div>
     );
 }
